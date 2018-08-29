b969b69fb111af66e3fa545cbfb38dcfdafbb99f
mubing.ye <mubing.ye>
2018-08-29 16:07:37 +0800

    链接数据库

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
-app.use((ctx) => {
-  ctx.body = "hello world";
+const bodyParser = require("koa-bodyparser");
+let MongoClient = require('mongodb').MongoClient;
+
+app.use(bodyParser());
+
+app.use(async ctx => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS');
+
+  if (ctx.path === "/save" && ctx.method === "GET") {
+    const reqBody = ctx.request.query;
+    await MongoClient.connect('mongodb://127.0.0.1:27017', {useNewUrlParser:true}, (err, db) => {
+      if (err) throw err;
+      const list = db.db('qrcode').collection('users');
+      list.find({"phoneNo": reqBody.phoneNo}).toArray((err, result) => {
+        if (err) throw err;
+        if (result.length <= 0) {
+          list.insertOne(reqBody, (err, result) => {
+            if (err) throw err;
+            ctx.body = 'success';
+          });
+        }else {
+          ctx.body = 'user has register'
+        }
+      });
+    });
+  } else {
+    ctx.body = "<h2>404</h2>";
+  }
 });
-app.listen(8099);
-console.log("德玛西亚");
+
+app.listen(8099, () => {
+  console.log("server is run");
+});
