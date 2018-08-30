8665f5d360cf2c6cb0a5e77e81a1f1b11bd8d393
mubing.ye <mubing.ye>
2018-08-30 17:34:01 +0800

    删除node_modules

--- index.js
+++ index.js
@@ -1,58 +1,72 @@
 const Koa = require("koa");
 const app = new Koa();
 const bodyParser = require("koa-bodyparser");
-let MongoClient = require('mongodb').MongoClient;
+let MongoClient = require("mongodb").MongoClient;
 
+function padLeftZero(str) {
+  return ("00" + str).substr(str.length);
+}
 const FormatDate = (date, fmt) => {
   if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+    fmt = fmt.replace(
+      RegExp.$1,
+      (date.getFullYear() + "").substr(4 - RegExp.$1.length)
+    );
   }
   let o = {
-    'M+': date.getMonth() + 1,
-    'd+': date.getDate(),
-    'h+': date.getHours(),
-    'm+': date.getMinutes(),
-    's+': date.getSeconds()
-  }
+    "M+": date.getMonth() + 1,
+    "d+": date.getDate(),
+    "h+": date.getHours(),
+    "m+": date.getMinutes(),
+    "s+": date.getSeconds()
+  };
   for (let k in o) {
     if (new RegExp(`(${k})`).test(fmt)) {
-      let str = o[k] + ''
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str))
+      let str = o[k] + "";
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? str : padLeftZero(str)
+      );
     }
   }
-  return fmt
-}
+  return fmt;
+};
 
 app.use(bodyParser());
 
 app.use(async ctx => {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS');
+  ctx.set("Access-Control-Allow-Origin", "*");
+  ctx.set(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS"
+  );
 
   if (ctx.path === "/save" && ctx.method === "GET") {
     const reqBody = ctx.request.query;
-    await MongoClient.connect('mongodb://127.0.0.1:27017', {useNewUrlParser:true}, (err, db) => {
-      if (err) throw err;
-      const list = db.db('qrcode').collection('users');
-      list.find({"phoneNo": reqBody.phoneNo}).toArray((err, result) => {
-        if (err) throw err;
-        if (result.length <= 0) {
-          reqBody.dt = +new Date();
-          reqBody.formatDT = FormatDate(new Date(), 'yyyy-MM-dd hh:mm:ss');
-          list.insertOne(reqBody, (err, result) => {
-            if (err) throw err;
-            ctx.body = 'success';
-          });
-        }else {
-          ctx.body = 'user has register'
-        }
-      });
-    });
+
+    const db = await MongoClient.connect(
+      "mongodb://127.0.0.1:27017",
+      { useNewUrlParser: true }
+    );
+
+    const col = await db.db("qrcode").collection("users");
+
+    const list = await col.find({"phoneNo": reqBody.phoneNo}).toArray();
+    if (list.length > 0) {
+      ctx.body = "当前手机号已经存在";
+    } else {
+      reqBody.dt = +new Date();
+      reqBody.formatDT = FormatDate(new Date(), "yyyy-MM-dd hh:mm:ss");
+      col.insertOne(reqBody);
+      ctx.body = "添加成功";
+    }
   } else {
     ctx.body = "<h2>404</h2>";
   }
 });
 
-app.listen(80, () => {
-  console.log("server is run");
+const port = 80;
+
+app.listen(port, () => {
+  console.log(`server is run at http://localhost:${port}`);
 });
