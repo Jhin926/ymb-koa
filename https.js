3465130388bacd197b10c2b2e9959674d466bda1
yemubing <yemubing@qq.com>
2018-08-31 17:31:07 +0800

    设置ssl证书

--- https.js
+++ https.js
@@ -2,13 +2,14 @@ const https = require('https');
 const fs = require('fs');
 
 const options = {
-  key: fs.readFileSync('./ali.key'),
-  cert: fs.readFileSync('./ali.pem')
+  key: fs.readFileSync('./214961025560978.key'),
+  ca: fs.readFileSync('./chain.pem'),
+  cert: fs.readFileSync('./public.pem')
 };
 
 https.createServer(options, (req, res) => {
   res.writeHead(200);
   res.end('hello world\n');
-}).listen(443, function() {
+}).listen(80, function() {
   console.log('server is run');
 });
