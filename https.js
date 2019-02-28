fbff80d8823b5d4953a38672f0a0f4ea3c9a1b64
mubing.ye <mubing.ye>
2019-02-28 16:26:42 +0800

    ali

--- https.js
+++ https.js
@@ -1,13 +1,17 @@
 const https = require('https');
 const fs = require('fs');
 
-const static = require('koa-static');
-app.use(static(__dirname + './www'))
-
+// const options = {
+//   pfx: fs.readFileSync('./keys/1272070_www.yemb.ren.pfx'),
+//   passphrase: 'pfx-password.txt'
+// };
+// https.createServer(options, (req, res) => {
+//   res.writeHead(200);
+//   res.end('hello world\n');
+// }).listen(8000);
 const options = {
-  key: fs.readFileSync('./214961025560978.key'),
-  ca: fs.readFileSync('./chain.pem'),
-  cert: fs.readFileSync('./public.pem')
+  key: fs.readFileSync('./keys/1870777_www.yemb.ren.key'),
+  cert: fs.readFileSync('./keys/1870777_www.yemb.ren.pem')
 };
 
 https.createServer(options, (req, res) => {
